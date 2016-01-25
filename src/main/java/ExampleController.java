1b9c7a4b530e18c6be6471b5f56cfaf61e4857b3
karsten <karsten.weber@immobilienscout24.de>
2016-01-25 18:01:23 +0100

    initial commit

---
+++ src/main/java/ExampleController.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class ExampleController {
+
+  @Controller
+  @EnableAutoConfiguration
+  public static class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Test!";
+    }
+
+    public static void main(String[] args) throws Exception {
+      SpringApplication.run(SampleController.class, args);
+    }
+  }
+}
