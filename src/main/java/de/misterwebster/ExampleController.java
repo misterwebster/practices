4f9dc66217abfa93e428c0c8f582ab39f99381bd
karsten <karsten.weber@immobilienscout24.de>
2016-01-25 18:15:47 +0100

    get example controller running

--- src/main/java/de/misterwebster/ExampleController.java
+++ src/main/java/de/misterwebster/ExampleController.java
@@ -1,4 +1,4 @@
-package main;
+package de.misterwebster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -6,12 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-public class ExampleController {
 
+public class ExampleController {
   @Controller
   @EnableAutoConfiguration
   public static class SampleController {
-
     @RequestMapping("/")
     @ResponseBody
     String home() {
